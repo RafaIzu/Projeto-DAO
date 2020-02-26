eca0180a6906d6d082272924ff916af79be02f83
RafaIzu <bomizukawa@gmail.com>
2020-02-26 20:19:39 -0300

    findById implementation

--- src/applicaton/Program.java
+++ src/applicaton/Program.java
@@ -13,12 +13,11 @@ public class Program {
 		
 		
 		Department obj=new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller=new Seller(555, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
-		System.out.println(seller);
-		
+				
 		SellerDao sellerDao=DaoFactory.createSellerDao();
+		Seller seller=sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
